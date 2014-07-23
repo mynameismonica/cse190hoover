c5aba23a126b6dc13b1125e295d413c7ddbd6910
mynameismonica <mgonsalv@ucsd.edu>
2014-07-23 13:53:13 -0700

    added level

--- src/com/example/memgame/MainActivity.java
+++ src/com/example/memgame/MainActivity.java
@@ -40,7 +40,8 @@ public class MainActivity extends ActionBarActivity {
 	public int numDigitsProcessing;             // The number of digits in the phone number the user is currently being prompted
 	                                            // to guess
 	
-	public int score;                           // The user's score. 
+	public int score;                           // The user's score.
+	public int level; 
 	
 	public String sequenceUserEntered;          // The sequence of digits the user has entered for this round
 	//public boolean userEnteringSequence;
@@ -51,8 +52,13 @@ public class MainActivity extends ActionBarActivity {
 	//public HashMap<String, Button> keypad; 
 	public String phoneNumber = "6955250";
 	
+	
+	// Makes x buttons blink in sequence, where x is the current number of digits in the
+	// phone number the user is being prompted to remember. 
 	public void highlightNumberSequence(View v)
 	{
+		// The main purpose of this hashmap is to make it easier to select
+		// which buttons to animate
 		HashMap<String, Integer> keypadButtons = new HashMap<String,Integer>();
 		keypadButtons.put("1",R.id.button1);
 		keypadButtons.put("2",R.id.button2);
@@ -65,6 +71,9 @@ public class MainActivity extends ActionBarActivity {
 		keypadButtons.put("9", R.id.button9);
 		keypadButtons.put("0", R.id.button11);
 		
+		// The next few lines creates a list of buttons and associates this 
+		// list with an animator set that makes it easy to animate 
+		// the set of buttons sequentially. 
 		List<Animator> phoneNumberSubsequence = new ArrayList<Animator>();
 	
 		for(int i = 0; i < numDigitsProcessing; i++)
@@ -75,22 +84,68 @@ public class MainActivity extends ActionBarActivity {
 		    anim.setTarget((Button) this.findViewById(keypadButtons.get(ch)));
 		}
 		
-		AnimatorSet as = new AnimatorSet();
 		
-		if(updateScore)
+		AnimatorSet as = new AnimatorSet();
+		as = updateScoreFn(v,as);
+		
+		if(addDigitToSequence)
 		{
+			ObjectAnimator levelAnimator = (ObjectAnimator)AnimatorInflater.loadAnimator(this, R.anim.blink);
+			levelAnimator.setTarget((TextView) this.findViewById(R.id.textView2));
+			levelAnimator.setDuration(700);
+			levelAnimator.start();
+			// update the score ones it comes into focus
+			TextView lvl = (TextView) this.findViewById(R.id.textView2);
+			level++;
+			lvl.setText(String.valueOf(level));
+		    as.setStartDelay(1700);
+		}
+		
+		/*if(updateScore)
+		{
+			as = updateScoreFn(v,as);
+			
+			// Using the blink animation defined in the resources folder, make the score 
+			// fade until it is transparent. 
 			ObjectAnimator animateScore = (ObjectAnimator)AnimatorInflater.loadAnimator(this, R.anim.blink);
 			animateScore.setDuration(700);
 			TextView pts = (TextView) this.findViewById(R.id.points);
 			animateScore.setTarget(pts);
 			animateScore.start();
+			// update the score ones it comes into focus
 			pts.setText(String.valueOf(score));
-		    as.setStartDelay(1700);	
-		}
+		   
+			// make sure the button sequence animation runs AFTER the 
+			// score has been updated.
+			as.setStartDelay(1700);	
+		}*/
 		
+		// Run the button sequence!
 		as.playSequentially(phoneNumberSubsequence);
 		as.setDuration(1000);
 		as.start();
+	}
+	
+	public AnimatorSet updateScoreFn(View v, AnimatorSet as)
+	{
+		if(!updateScore)
+			return as; 
+		
+		// Using the blink animation defined in the resources folder, make the score 
+		// fade until it is transparent. 
+		ObjectAnimator animateScore = (ObjectAnimator)AnimatorInflater.loadAnimator(this, R.anim.blink);
+		animateScore.setDuration(700);
+		TextView pts = (TextView) this.findViewById(R.id.points);
+		animateScore.setTarget(pts);
+		animateScore.start();
+		// update the score ones it comes into focus
+		pts.setText(String.valueOf(score));
+	   
+		// make sure the button sequence animation runs AFTER the 
+		// score has been updated.
+		as.setStartDelay(1700);			
+		
+		return as; 
 	}
 	
 	public void enteredNumber(View v)
@@ -157,12 +212,15 @@ public class MainActivity extends ActionBarActivity {
 	
 	public boolean validSequenceEntered()
 	{
-		for(int i = 0; i < numDigitsProcessing; i++)
+		String phoneNumberSubsequencePrompted = phoneNumber.substring(0, numDigitsProcessing);
+		return phoneNumberSubsequencePrompted.equals(sequenceUserEntered);
+			
+		/*for(int i = 0; i < numDigitsProcessing; i++)
 		{
 			if(sequenceUserEntered.charAt(i) != phoneNumber.charAt(i) )
 				return false; 
 		}
-		return true; 
+		return true; */
 	}
 	
 	@Override
@@ -174,6 +232,7 @@ public class MainActivity extends ActionBarActivity {
 		//userEnteringSequence = false;
 		addDigitToSequence = true; 
 		updateScore = false; 
+		level = 0; 
 		score = 0; 
 
 		super.onCreate(savedInstanceState);
